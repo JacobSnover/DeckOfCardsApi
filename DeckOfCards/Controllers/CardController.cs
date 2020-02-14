1033f797b95e1adf4051c78ccac205378a6fc703
Jacob R. Snover <snoverjacob@yahoo.com>
2020-02-13 21:03:14 -0500

    First Commit

---
+++ DeckOfCards/Controllers/CardController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeckOfCards.Controllers
+{
+    public class CardController : Controller
+    {
+        // this JsoNDocument will eventually hold our cards
+        //after we draw from the deck
+        private JsonDocument jDoc;
+
+        // make a method that will draw a new deck
+        // this will give us access to a deck id
+        public async Task<string> GetDeck()
+        {
+            // make using block to hold our HttpClient object
+            using (var httpClient = new HttpClient())
+            {
+                // we make another using block to make an async call to the api
+                using (var response = await httpClient
+                    .GetAsync("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"))
+                {
+                    // read our api response as a string
+                    var stringResponse = await response.Content.ReadAsStringAsync();
+                    // parse the string and return a JsonDocument
+                    jDoc = JsonDocument.Parse(stringResponse);
+                }
+            }
+
+            // we return the deck id as a string,
+            //that we will get from a property of our JsonDocument
+            return jDoc.RootElement.GetProperty("deck_id").GetString();
+        }
+
+        // we make an async method that will call the deck of cards api
+        // and return to use a number of cards
+        // we will put those in a List of cards, and return them
+        public async Task<List<Card>> GetCards(string deckId)
+        {
+            // make a blank list of cards to add cards to later
+            List<Card> cardList = new List<Card>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient
+                    .GetAsync("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=5"))
+                {
+                    var stringResponse = await response.Content.ReadAsStringAsync();
+                    jDoc = JsonDocument.Parse(stringResponse);
+
+                    // this grabbed the data the formed our card array in Json format
+                    var jsonList = jDoc.RootElement.GetProperty("cards");
+
+                    // wee will use a for loop to iterate through that card array
+                    //and build our List of cards to return
+                    for(int i = 0; i < jsonList.GetArrayLength(); i++)
+                    {
+                        // we parse through the data while building a new Card
+                        // and then add that Card to our List of cards
+                        cardList.Add(new Card() 
+                        { 
+                            // we now map the properties from the Json
+                            // to the new Card and add to the List
+                            image = jsonList[i].GetProperty("image").GetString(),
+                            suit = jsonList[i].GetProperty("suit").GetString(),
+                            value = jsonList[i].GetProperty("value").GetString(),
+                            code = jsonList[i].GetProperty("code").GetString()
+                        
+                        });
+                    }
+
+
+
+                }
+            }
+            return cardList;
+        }
+
+
+    }
+}
---
+++ DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DeckOfCards.Models;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        // make a global card controller object, that allows me to call
+        // the deck of cards api
+        CardController cardController = new CardController();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var deckid = await cardController.GetDeck();
+
+            // we use sessions to hold data as a key value pair
+            HttpContext.Session.SetString("deckId", deckid);
+
+            //var testSession = HttpContext.Session.GetString("deckId");
+
+            return View();
+        }
+
+        // we make an async method that will call the cardController
+        // and return to us a number of cards
+        public async Task<IActionResult> GetCards()
+        {
+            List<Card> cardList = await cardController
+                .GetCards(HttpContext.Session.GetString("deckId"));
+            return View(cardList);
+        }
+
+
+
+        ////we make the method async, so that we can await async method calls
+        ////inside this method
+        //// Task keyword is saying that the return process will be an async process
+        //public async Task<IActionResult> Index()
+        //{
+        //    //using blocks in our code, assure that the objects resources/memory 
+        //    //are, released to be used again
+
+        //    //using this string to hold response before it gets released
+        //    string apiResponse = "";
+
+        //    //make an new HttpClient object in a using block
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        //make another using block for these internal processes
+        //        //call the GetAsync method, and await the response
+        //        using (var response = await httpClient.GetAsync("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"))
+        //        {
+        //            //we then read the response with the ReadAsStringAsync method
+        //            //which we will also await
+        //            apiResponse = await response.Content.ReadAsStringAsync();
+
+        //            //parses our Json String into a JsonDocument object
+        //            var jsonDocument = JsonDocument.Parse(apiResponse);
+
+        //            // we can then GetProperty or "Index" through our JsonDocument
+        //            //object, and then we call the GetString method,
+        //            //to retrieve the value from that index
+        //            //we call GetString because the value is a string,
+        //            //if the value is another type, you have to call
+        //            //the method that represents that type
+        //            var jsonProperty = jsonDocument.RootElement
+        //                .GetProperty("deck_id").GetString();
+
+        //            var cardModel = JsonSerializer.Deserialize<Card>(apiResponse);
+
+        //        }// this releases the resource used to call the api
+
+        //    }// this releases the memory used to hold the HttpClient
+
+        //        return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
